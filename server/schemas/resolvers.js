6a44af41611783c5058e8a05213cbce5ff3d7bbd
PeteChicchetti <pj0524@gmail.com>
2022-12-08 18:33:11 -0500

    added 'me' query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,3 +2,16 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
+const resolvers = {
+    Query: {
+      me: async (parent, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id })
+        }
+        throw new AuthenticationError('You need to be logged in!');
+      },
+    },
+
+    Mutation: {
+        
+    }
