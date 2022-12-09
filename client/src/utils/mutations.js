e54bd6559727052f51b9a2de420e5eefc75e7703
PeteChicchetti <pj0524@gmail.com>
2022-12-08 19:38:16 -0500

    added savebook mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -33,3 +33,21 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: InputBook!) {
+    saveBook(newBook: $newBook) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            author
+            description
+            title
+            image
+            link
+      }
+    }
+  }
+`;
