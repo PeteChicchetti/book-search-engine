1c4eb936d579c9c7471420b430d281c7c68befa2
PeteChicchetti <pj0524@gmail.com>
2022-12-08 19:14:40 -0500

    added mutation to typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,6 +27,13 @@ const typeDefs = gql`
     me: User
   }
 
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
 `;
 
 module.exports = typeDefs;
